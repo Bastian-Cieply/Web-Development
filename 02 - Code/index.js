d06eb8d1e572d2642715cdd9ff0e662c6b7a779b
Bastian-Cieply <bastianciep@gmail.com>
2024-02-22 16:10:24 +0100

    Backend Login and Registration
    Enhanced the DB so that user can be saved. The login and registration of new accounts should be implemented. The password is SHA encrypted with SALT. Also added a cookie that will be set once the user is logged in or has registered his account.

--- 02 - Code/index.js
+++ 02 - Code/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -7,9 +8,39 @@ const port = 3000;
 const ModelClass = require('./model.js');
 const Model = new ModelClass();
 
+app.use(cookieParser());
+
 app.get('/', async (req, res) => {
   const stores = await Model.getStores();
   res.json(stores);
+});
+
+app.get('/login', async (req, res) => {
+  const { email, password } = req.query;
+  const status = await Model.userLogin(email, password);
+  if  (status) {
+    res.cookie('session', 'my-session-cookie', { httpOnly: true});
+    res.json({ message: "Login successful" });
+  } else {
+    res.status(401).json({ message: "Login failed" });
+  }
+});
+
+app.get('/register', async (req, res) => {
+  const { email, password } = req.query;
+  const status = await Model.registerUser(email, password);
+  if (status.status === 'Success') {
+    res.cookie('session', 'my-session-cookie', { httpOnly: true});
+  }
+  res.json(status);
+});
+
+app.get('/user-logged-in', async(req, res) => {
+  if (req.cookies.session === 'my-session-cookie') {
+    res.send({ message: "User is logged in" });
+  } else {
+    res.status(401).send({ message: "User is not logged in" });
+  }
 });
 
 const server = async () => {
--- 02 - Code/model.js
+++ 02 - Code/model.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg');
 
 const stores = require('./stores.json');
+const crypto = require('crypto');
 
 class ModelClass {
   constructor() {
@@ -34,7 +35,6 @@ class ModelClass {
           OWNER to postgres
     `);
 
-
     for (const store of stores) {
 
       const { rows } = await this.connection.query(`
@@ -49,6 +49,21 @@ class ModelClass {
         `, [store.name, store.url, store.district]);
       }
     }
+
+    await this.connection.query(`
+    CREATE TABLE IF NOT EXISTS public.users
+    (
+        id SERIAL,
+        email text UNIQUE,
+        salt text,
+        password text,
+        CONSTRAINT user_pkey PRIMARY KEY (id)
+    )`);
+
+    await this.connection.query(`
+      ALTER TABLE IF EXISTS public.users
+          OWNER to postgres
+    `);
   }
 
   async getStores() {
@@ -58,6 +73,43 @@ class ModelClass {
     return rows;
   }
 
+  async registerUser(email, password) {
+    const salt = crypto.randomBytes(16).toString('hex');
+    const hashedPassword = crypto
+    .createHash('sha512')
+    .update(email + salt + password)
+    .digest('hex');
+
+    try {
+      await this.connection.query(`
+        INSERT INTO users (email, salt, password)
+        VALUES ($1, $2, $3)
+      `, [email, salt, hashedPassword]);
+      return { status: 'Success', message: 'User registered successfully' };
+    } catch (error) {
+      return { status: 'Error', message: error.message };
+    }
+  }
+
+  async userLogin(email, password) {
+    const { rows } = await this.connection.query(`
+      SELECT * FROM users WHERE email = $1
+    `, [email]);
+
+    if (rows.length === 1) {
+      const user = rows[0];
+      const hashedPassword = crypto
+        .createHash('sha512')
+        .update(email + user.salt + password)
+        .digest('hex');
+
+      if (hashedPassword === user.password) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
 
 }
 
