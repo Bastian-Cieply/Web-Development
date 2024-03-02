16219739b0147d1988de82d36abd95124f47f366
Bastian-Cieply <bastianciep@gmail.com>
2024-03-02 17:04:56 +0100

    Implemented landing page
    Little landing page

--- 02 - Code/public/start.js
+++ 02 - Code/public/start.js
@@ -1,19 +1,9 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+    toggleSidebar();
+}
 
-
-console.log('Hello');
-
-fetch("http://localhost:3000/stores")
-.then(response => response.json())
-.then(stores => {
-    stores.forEach(store => {
-        Object.values(store).forEach(value => {
-            const element = document.createElement('div');
-            element.textContent = value;
-            document.body.appendChild(element);
-        });
-    });
-});
-
-
-
-
+function toggleSidebar() {
+    var sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("active");
+}
