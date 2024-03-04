884a70e60401940dadb431f4a18a8d405c01367f
JasperFVKG <130658890+JasperFVKG@users.noreply.github.com>
2024-03-04 15:51:05 +0100

    Dynamic Text
    Added Dynamic Text on landing page

--- 02 - Code/public/start.js
+++ 02 - Code/public/start.js
@@ -7,3 +7,28 @@ function toggleSidebar() {
     var sidebar = document.getElementById("sidebar");
     sidebar.classList.toggle("active");
 }
+
+window.onload = function randomText() {
+    // Array of different texts
+    var texts = [
+      "Embrace the spirit of Småland!",
+      "Also try Göteborg!",
+      "The best of the Köpings!",
+      "Limited edition!",
+      "Made in Sweden!",
+      "Wow!",
+      "In color!",
+      "Now with more ducks!",
+      ""
+    ];
+
+    // Select the paragraph element
+    var paragraph = document.getElementById("dynamic-text");
+
+    // Choose a random text from the array
+    var randomIndex = Math.floor(Math.random() * texts.length);
+    var newText = texts[randomIndex];
+
+    // Set the text of the paragraph
+    paragraph.textContent = newText;
+  };
